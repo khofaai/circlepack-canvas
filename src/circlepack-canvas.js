d652949b0c4445b03af61fabc821162205292b6a
Anas El Alami <anaselalamikh@gmail.com>
2022-12-10 15:39:04 +0100

    0.1.0-alpha2:WIP: adding hover change node attribute logic

--- src/circlepack-canvas.js
+++ src/circlepack-canvas.js
@@ -2,27 +2,20 @@ import d3 from 'd3'
 
 const defaultOptions = {
   padding: 1,
-  colorRange: ['#9677FF', '#CFC1FB', '#655E7A', '#765DC7', '#4A3A7A', '#A599C7', '#9475FA', '#FFFFFF'],
-  value: node => node.size,
-  sort: node => node.ID,
-  dataset: [],
-  initCirclesDuration: 2000,
+  colorRange: ['#bfbfbf','#838383','#4c4c4c','#1c1c1c'],
+  value: (d) => d.size,
+  sort: (d) => d.ID,
+  dataset: []
 }
 
 export default (selector, options = {}) => {
   const $options = Object.assign({}, defaultOptions, options )
-  //////////////////////////////////////////////////////////////
-  ////////////////// Create Set-up variables  //////////////////
-  //////////////////////////////////////////////////////////////
   const domElement = document.querySelector(selector)
   const width = Math.max(domElement.clientWidth, 350) - 20;
   const height = (window.innerWidth < 768 ? width : window.innerHeight - 20);
 
   const centerX = width / 2;
   const centerY = height / 2;
-  //////////////////////////////////////////////////////////////
-  /////////////////////// Create SVG  ///////////////////////
-  //////////////////////////////////////////////////////////////
 
   //Create the visible canvas and context
   const canvas  = d3.select(selector).append('canvas')
@@ -46,51 +39,40 @@ export default (selector, options = {}) => {
   //Create a custom element, that will not be attached to the DOM, to which we can bind the data
   const detachedContainer = document.createElement('custom');
   d3.select(detachedContainer);
-  const dataContainer = d3.select(detachedContainer);
-
-  //////////////////////////////////////////////////////////////
-  /////////////////////// Create Scales  ///////////////////////
-  //////////////////////////////////////////////////////////////
 
   const colorCircle = d3.scale.ordinal().domain([]).range($options.colorRange);
 
-  const diameter = Math.min(width * 0.9, height * 0.9)
+  const diameter = Math.min(width*0.9, height*0.9)
+
+  const zoomInfo = {
+    centerX: width / 2,
+    centerY: height / 2,
+    scale: 1
+  };
 
   //Dataset to swtich between color of a circle (in the hidden canvas) and the node data
   const colToCircle = {};
+
   const pack = d3.layout.pack()
     .padding($options.padding)
     .size([diameter, diameter])
     .value($options.value)
     .sort($options.sort);
 
-  //////////////////////////////////////////////////////////////
-  ////////////////// Create Circle Packing /////////////////////
-  //////////////////////////////////////////////////////////////
-
-  const nodes = pack.nodes($options.dataset);
-  const root = $options.dataset;
-  const focus = root;
-
-  console.log('NODES COUNT:', nodes.length)
-
-  dataContainer.selectAll('.node')
-    .data(nodes)
-    .enter().append('circle')
-    .attr('id', (d,i) => 'nodeCircle_' + i)
-    .attr('class', (d/* , i */) => d.parent ? d.children ? 'node' : 'node node--leaf' : 'node node--root')
-    .attr('r', (d) => d.r)
-    .attr('cx', 0)
-    .attr('cy', 0)
-    .attr('fill', (d) => d.children ? colorCircle(d.depth) : 'white');
-
-  //////////////////////////////////////////////////////////////
-  ///////////////// Canvas draw function ///////////////////////
-  //////////////////////////////////////////////////////////////
+  const nodes = pack.nodes($options.dataset),
+    root = $options.dataset,
+    focus = root;
 
   const cWidth = canvas.attr('width');
   const cHeight = canvas.attr('height');
-  // const nodeCount = nodes.length;
+  const nodeCount = nodes.length;
+
+  const getNodeColor = (node) => {
+    // hiddenContext.fillStyle = grd;
+    // hiddenContext.fillRect(20, 20, 150, 100);
+    if (node.highlight) return node.highlight
+    return node.children ? colorCircle(node.depth) : 'white';
+  }
 
   //The draw function of the canvas that gets called on each frame
   function drawCanvas(chosenContext, hidden) {
@@ -100,80 +82,121 @@ export default (selector, options = {}) => {
     chosenContext.rect(0,0,cWidth,cHeight);
     chosenContext.fill();
 
-    const elements = dataContainer.selectAll('.node');
-    elements.each(function() {
-				const node = d3.select(this);
-				//If the hidden canvas was send into this function
-				//and it does not yet have a color, generate a unique one
-				if(hidden) {
-					if(node.attr('color') === null) {
-            // If we have never drawn the node to the hidden canvas get a new color for it and put it in the dictionary.
-            node.attr('color', genColor());
-            colToCircle[node.attr('color')] = node;
-					}//if
-					// On the hidden canvas each rectangle gets a unique color.
-					chosenContext.fillStyle = node.attr('color');
-				} else {
-					chosenContext.fillStyle = node.attr('fill');
-				}//else
+    //Select our dummy nodes and draw the data to canvas.
+    var node = null;
+    // It's slightly faster than nodes.forEach()
+    for (var i = 0; i < nodeCount; i++) {
+      node = nodes[i];
 
-				//Draw each circle
-				chosenContext.beginPath();
-				chosenContext.arc((centerX + +node.attr('cx')), (centerY + +node.attr('cy')), node.attr('r'), 0,  2 * Math.PI, true);
-				chosenContext.fill();
-				chosenContext.closePath();
-    })
+      //If the hidden canvas was send into this function and it does not yet have a color, generate a unique one
+      if(hidden) {
+        if(node.color == null) {
+          // If we have never drawn the node to the hidden canvas get a new color for it and put it in the dictionary.
+          node._id = i + 1
+          node.color = genColor();
+          colToCircle[node.color] = node;
+        }//if
+        // On the hidden canvas each rectangle gets a unique color.
+        chosenContext.fillStyle = node.color;
+      } else {
+        chosenContext.fillStyle = getNodeColor(node)
+      }//else
+
+      //Draw each circle
+      chosenContext.beginPath();
+      chosenContext.arc(((node.x - zoomInfo.centerX) * zoomInfo.scale) + centerX,
+                ((node.y - zoomInfo.centerY) * zoomInfo.scale) + centerY,
+                node.r * zoomInfo.scale, 0,  2 * Math.PI, true);
+      chosenContext.fill();
+
+    }//for i
+
   }
   //function drawCanvas
 
-  //////////////////////////////////////////////////////////////
-  /////////////////// Mouse functionality //////////////////////
-  //////////////////////////////////////////////////////////////
-
   const canvasDom = document.getElementById('canvas')
   // Listen for clicks on the main canvas
-  // canvasDom.addEventListener('click', (e) => {
-  //   // We actually only need to draw the hidden canvas when there is an interaction.
-  //   // This sketch can draw it on each loop, but that is only for demonstration.
-  //   drawCanvas(hiddenContext, true);
+  canvasDom.addEventListener('click', (e) => {
+    // We actually only need to draw the hidden canvas when there is an interaction.
+    // This sketch can draw it on each loop, but that is only for demonstration.
+    drawCanvas(hiddenContext, true);
 
-  //   const node = getNode(e)
-  //   if (focus !== node) zoomToCanvas(node);
-  //   else zoomToCanvas(root);
-  // });
+    //Figure out where the mouse click occurred.
+    const node = getNode(e)
 
-  // canvasDom.addEventListener('mousemove', (e) => {
-  //   // We actually only need to draw the hidden canvas when there is an interaction.
-  //   // This sketch can draw it on each loop, but that is only for demonstration.
-  //   drawCanvas(hiddenContext, true);
+    if(node) {
+      if (focus !== node) zoomToCanvas(node); else zoomToCanvas(root);
+    } else {
+      zoomToCanvas(root);
+    }
+  });
 
-  //   //Figure out where the mouse click occurred.
-  //   const node = getNode(e)
-  //   if(node) {
-  //     const span = document.getElementById('tooltip')
-  //     span.innerText = focus !== node ? node.name : ''
-  //   }//if
-  // });
+  canvasDom.addEventListener('mousemove', (e) => {
+    // We actually only need to draw the hidden canvas when there is an interaction.
+    // This sketch can draw it on each loop, but that is only for demonstration.
+    drawCanvas(hiddenContext, true);
 
-  //////////////////////////////////////////////////////////////
-  ///////////////////// Zoom Function //////////////////////////
-  //////////////////////////////////////////////////////////////
+    //Figure out where the mouse click occurred.
+    const node = getNode(e)
+
+    for (var i = 0; i < nodeCount; i++) {
+      const _node = nodes[i];
+      _node.highlight = null
+    }
+    const span = document.getElementById('tooltip')
+    if(node) {
+      node.highlight = '#f00'
+      if (node.name !== '') {
+        span.style.display = 'block'
+        span.style.left = (e.layerX - 20) + 'px'
+        span.style.top = (e.layerY - 50) + 'px'
+      }
+      if (focus !== node) {
+        span.innerText = node.name
+      } else {
+        span.innerText = ''
+        span.style.display = 'none'
+      }
+    } else {
+      span.style.display = 'none'
+    }
+  });
 
   //Based on the generous help by Stephan Smola
   //http://bl.ocks.org/smoli/d7e4f9199c15d71258b5
 
+  const ease = d3.ease('cubic-in-out');
+  let duration = 2000;
+  let timeElapsed = 0;
+  let interpolator = null;
+  let vOld = [focus.x, focus.y, focus.r * 2.05];
+
+  //Create the interpolation function between current view and the clicked on node
   const zoomToCanvas = (focusNode) => {
     const focus = focusNode;
     const v = [focus.x, focus.y, focus.r * 2.05]; //The center and width of the new 'viewport'
-    const k = diameter / v[2]
-    // interpolator = d3.interpolateZoom(vOld, v); //Create interpolation between current and new 'viewport'
-    dataContainer.selectAll('.node')
-      .transition()
-      .duration($options.initCirclesDuration)
-      .attr('cx', d => (d.x - v[0]) * k)
-      .attr('cy', d => (d.y - v[1]) * k)
-      .attr('r', d => d.r * k);
+
+    interpolator = d3.interpolateZoom(vOld, v); //Create interpolation between current and new 'viewport'
+
+    duration = 	interpolator.duration; //Interpolation gives back a suggested duration
+    timeElapsed = 0; //Set the time elapsed for the interpolateZoom function to 0
+    vOld = v; //Save the 'viewport' of the next state as the next 'old' state
   }
+  //function zoomToCanvas
+
+  //Perform the interpolation and continuously change the zoomInfo while the 'transition' occurs
+  const interpolateZoom = (dt) => {
+    if (interpolator) {
+      timeElapsed += dt;
+      const t = ease(timeElapsed / duration);
+
+      zoomInfo.centerX = interpolator(t)[0];
+      zoomInfo.centerY = interpolator(t)[1];
+      zoomInfo.scale = diameter / interpolator(t)[2];
+
+      if (timeElapsed >= duration) interpolator = null;
+    }//if
+  }//function zoomToCanvas
 
   //Generates the next color in the sequence, going from 0,0,0 to 255,255,255.
   //From: https://bocoup.com/weblog/2d-picking-in-canvas
@@ -193,8 +216,7 @@ export default (selector, options = {}) => {
   }
   //function genColor
 
-  const getCircle = (e) => {
-    //Figure out where the mouse click occurred.
+  const getNode = (e) => {
     const mouseX = e.layerX;
     const mouseY = e.layerY;
 
@@ -203,63 +225,26 @@ export default (selector, options = {}) => {
     const col = hiddenContext.getImageData(mouseX, mouseY, 1, 1).data;
     //Our map uses these rgb strings as keys to nodes.
     const colString = 'rgb(' + col[0] + ',' + col[1] + ','+ col[2] + ')';
-    return colToCircle[colString]
+    return colToCircle[colString];
   }
-
-  const getNode = (e) => {
-    const node = getCircle(e)
-    if (node) {
-      return dataContainer.selectAll(`#${node.attr('id')}`)[0][0].__data__;
-    }
-    return null;
-  }
-
-  const addEvent = (eventName, callback = () => {}) => {
-    canvasDom.addEventListener(eventName, (e) => {
-        // We actually only need to draw the hidden canvas when there is an interaction.
-      // This sketch can draw it on each loop, but that is only for demonstration.
-      drawCanvas(hiddenContext, true);
-
-      //Figure out where the mouse click occurred.
-      const node = getNode(e)
-      if (node && callback) {
-        callback(node, e)
-      }
-    })
-  }
-
-  addEvent('click', (node) => {
-    if (focus !== node) zoomToCanvas(node);
-    else zoomToCanvas(root);
-  })
-
-  addEvent('mousemove', (node) => {
-    const span = document.getElementById('tooltip')
-    span.innerText = focus !== node ? node.name : ''
-  })
-
-  //////////////////////////////////////////////////////////////
-  /////////////////////// Initiate /////////////////////////////
-  //////////////////////////////////////////////////////////////
 
   //First zoom to get the circles to the right location
   zoomToCanvas(root);
-  const animate = () => {
+
+  let dt = 0;
+  d3.timer((elapsed) => {
+    // stats.begin();
+    interpolateZoom(elapsed - dt);
+    dt = elapsed;
+    // stats.end();
     drawCanvas(context);
-    window.requestAnimationFrame(animate);
-  }
-  window.requestAnimationFrame(animate);
+  });
+  drawCanvas(hiddenContext, true);
+
 
   return {
     zoomTo: zoomToCanvas,
-    zoomReset() {
-      zoomToCanvas(root)
-    },
-    hiddenCanvas,
     canvas: canvasDom,
-    getNode,
-    getCircle,
-    drawCanvas,
-    addEvent,
+    getNode
   }
 }
